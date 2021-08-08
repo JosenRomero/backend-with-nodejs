dbed423213184bcb9aaebbfad24d7789f9e2433f
jose manuel <romerojosemanuel@hotmail.com>
2021-08-08 16:33:37 -0600

    add express-session

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require('dotenv').config({path: './config/.env'});
 require("./config/database");
 const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 
@@ -13,8 +14,20 @@ app.set("port", process.env.PORT || 3001)
 
 app.use(express.json());
 
+app.use(session({
+    name: "session",
+    secret: process.env.SECRETCODE,
+    resave: true,
+    saveUninitialized: true,
+    secure: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
+}));
+
 // initalize passport
 app.use(passport.initialize()); 
+
+// deserialize cookie from the browser
+app.use(passport.session()); 
 
 // set up cors to allow us to accept requests from our client
 app.use(cors({
