d31f214938405d84ad4b9637ec3f8b83873d2c29
Jose <romerojosemanuel@hotmail.com>
2025-04-24 20:04:37 -0600

    remove unnecessary code

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ const sessionConfig = {
         sameSite: 'none',
         secure: true,
         httpOnly: true,
-        domain: process.env.CLIENT_URL,
         maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
     },
     store: MongoStore.create({ 
